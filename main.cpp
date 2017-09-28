7038409b33a1d24533d93d8bcf06fc263aa295ed
s274502 <s274502@sow767364f.misc.cranfield.ac.uk>
2017-09-28 13:01:26 +0100

    End of first lab

--- main.cpp
+++ main.cpp
@@ -18,40 +18,43 @@ int sign(double x){
 	}
 }
 
+double upwindScheme(double deltaX,double deltaT,double u){
+
+	double resultArray[5][100];
+
+	for(int i=0; i<100 ; i++){
+		resultArray[0][i] = f0(-40+deltaX*i);	
+	} 
+
+	for(int i=1; i<5 ; i++){
+		resultArray[i][0] = 0;
+		resultArray[i][99] = 1;
+		for(int j=1; j<99 ; j++){
+			resultArray[i][j] = -u*deltaT*((resultArray[i-1][j]-resultArray[i-1][j-1])/deltaX)+ resultArray[i-1][j];
+		}
+		cout << "\n";
+	}
+}
+
 
 int main(){
-int u = 1;
-cout << "Start\n";
-double previousAnswer = 0;
-double deltaX = 0.8;
-double deltaT = 0.5;
-//First entry is  time, second is space
-double resultArray[5][100];
 
-//The step is 0.8
+	cout << "Start\n";
+	double previousAnswer = 0;
+	double deltaX = 0.8;
+	double deltaT = 0.001;
+	int u = 1;
+	//First entry is  time, second is space
+	double[][] resultArray = upwindScheme(deltaX,deltaT,u);
 
-for(int i=0; i<100 ; i++){
-	resultArray[0][i] = f0(-40+deltaX*i);	
-} 
-
-for(int i=1; i<5 ; i++){
-	resultArray[i][0] = 0;
-	resultArray[i][99] = 1;
-	for(int j=1; j<99 ; j++){
-		resultArray[i][j] = -u*deltaT*((resultArray[i-1][j]-resultArray[i-1][j-1])/deltaX)+ resultArray[i-1][j];
-	}
-	cout << "\n";
-} 
-
-
-for(int i=0; i<5 ; i++){
-	for(int j=0; j<100 ; j++){
-		cout << resultArray[i][j] << " ";
-	}
-	cout << "\n";
-} 
-
-cout << "End\n";
+	//The step is 0.8
+	for(int i=0; i<5 ; i++){
+		for(int j=0; j<100 ; j++){
+			cout << resultArray[i][j] << " ";
+		}
+		cout << "\n";
+	} 
+	cout << "End\n";
 	
 }
 
